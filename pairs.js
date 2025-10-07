0c13b30323584573d9c4fa0f99eb35ccfae0c90a
A Dil <adilhasan2641@gmail.com>
2025-10-07 16:06:23 +0600

    Update pairs.js

--- pairs.js
+++ pairs.js
@@ -10,5 +10,33 @@
   {
     "boy": "https://i.postimg.cc/C5KDv1kz/52979d65eb8fb622c6f10ad6807fc292.jpg",
     "girl": "https://i.postimg.cc/85ggBf8Z/d6f2525d460d91fa1048157b96852a12.jpg"
-  }
+  },
+  {
+    "boy": "https://i.postimg.cc/76247vBf/bbcfd65e6ee58c313a1ae3e345bd889c.jpg",
+    "girl": "https://i.postimg.cc/vHSPR1Yj/49e2ecb7da39394dc8a4d59a745b1e57.jpg"
+  },
+   {
+    "boy": "https://i.postimg.cc/8kXdcq2g/4ca5f6b9fc2b4a622e16cd4593747e30.jpg",
+    "girl": "https://i.postimg.cc/BtWXDcc3/af6b1040aa44a0ed47529d6e6c4e199a.jpg"
+  },
+   {
+    "boy": "https://i.postimg.cc/G28Dy2wB/c72c578e6e6322e0c55a519a1de8f8ab.jpg",
+    "girl": "https://i.postimg.cc/WbPZdT9j/238b6f7cdd79be1db475d316afe5b9d1.jpg"
+  },
+   {
+    "boy": "https://i.postimg.cc/tCS9Lznf/558c4849f8b9ac85bb52f971d0a38286.jpg",
+    "girl": "https://i.postimg.cc/cHyxF4t6/af4406e47d05fc513176d86083f36c30.jpg"
+  },
+   {
+    "boy": "https://i.postimg.cc/1tHkD9pj/1dcd6e4c9703be43dfea197fd7a1b7da.jpg",
+    "girl": "https://i.postimg.cc/CMvy1xMb/9e751575be8a461374979b27fea5e89e.jpg"
+  },
+   {
+    "boy": "https://i.postimg.cc/gJkSF3hc/16314201cfb83da49b748ae2f82fc901.jpg",
+    "girl": "https://i.postimg.cc/bwkFQyPW/a6e8f7f7b857f6ae461fb2f21a7bb18f.jpg"
+  },
+   {
+    "boy": "https://i.postimg.cc/vBNvJkMY/d933bc5b36663049926c48017db28af0.jpg",
+    "girl": "https://i.postimg.cc/jjRvPyZw/d176006c1e9acc249f7036a961afbc74.jpg"
+  },
 ]
