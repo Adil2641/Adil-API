1fa65509bec02cfe4ef2d8d6a4ea54a127d1d1fe
A Dil <adilhasan2641@gmail.com>
2025-10-07 15:24:01 +0600

    Update pairs.js

--- pairs.js
+++ pairs.js
@@ -1,10 +1,10 @@
 [
   {
-    boy: "https://i.postimg.cc/Gh8jcnC6/23b0bb5de1d621524ae37c71220c0c33.jpg",
-    girl: "https://i.postimg.cc/mgs5cgVj/b96ef145bb5f8757b847a922202c9b60.jpg",
+    "boy": "https://i.postimg.cc/Gh8jcnC6/23b0bb5de1d621524ae37c71220c0c33.jpg",
+    "girl": "https://i.postimg.cc/mgs5cgVj/b96ef145bb5f8757b847a922202c9b60.jpg"
   },
   {
-    boy: "https://i.postimg.cc/HL2R18Dm/19d52569ecab92f3333803acf9438e7c.jpg",
-    girl: "https://i.postimg.cc/SNxKBCBx/20370532375aedea7f947813684bf8e9.jpg",
-  },
+    "boy": "https://i.postimg.cc/HL2R18Dm/19d52569ecab92f3333803acf9438e7c.jpg",
+    "girl": "https://i.postimg.cc/SNxKBCBx/20370532375aedea7f947813684bf8e9.jpg"
+  }
 ]
